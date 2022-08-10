fc35f9a738e0e06ca2ad55831521da8efae8399b
Agamenon <chams0099@gmail.com>
2022-08-10 17:25:32 +0100

    more work on the assembler

--- src/Assembler/Assembler.cpp
+++ src/Assembler/Assembler.cpp
@@ -50,11 +50,136 @@ void Assembler::assemble(std::string filename) {
             sline >> op1;
             sline >> op2;
             int reg, val;
-            if (op1[0] == 'v') {
-                reg = std::stoi(op1.substr(1, 1), 0, 16);
-                val = std::stoi(op2);
+            if (op1[0] != 'v') {
+                // error
             }
+            reg = std::stoi(op1.substr(1, 1), 0, 16);
+            val = std::stoi(op2);
             opcode = 0x6000 | ((reg & 0x000f) << 8) | (val & 0x00ff); // 0x6xnn
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "or") {
+            sline >> op1;
+            sline >> op2;
+            int reg1, reg2;
+            if(op1[0] != 'v' || op2[0] != 'v') {
+                // error
+            }
+            reg1 = std::stoi(op1.substr(1, 1), 0, 16);
+            reg2 = std::stoi(op2.substr(1, 1), 0, 16);
+            opcode = 0x8000 | ((reg1 & 0xf) << 8) | ((reg2 & 0xf) << 4) | 0x1; // 0x8xy1
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "and") {
+            sline >> op1;
+            sline >> op2;
+            int reg1, reg2;
+            if(op1[0] != 'v' || op2[0] != 'v') {
+                // error
+            }
+            reg1 = std::stoi(op1.substr(1, 1), 0, 16);
+            reg2 = std::stoi(op2.substr(1, 1), 0, 16);
+            opcode = 0x8000 | ((reg1 & 0xf) << 8) | ((reg2 & 0xf) << 4) | 0x2; // 0x8xy2
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "xor") {
+            sline >> op1;
+            sline >> op2;
+            int reg1, reg2;
+            if(op1[0] != 'v' || op2[0] != 'v') {
+                // error
+            }
+            reg1 = std::stoi(op1.substr(1, 1), 0, 16);
+            reg2 = std::stoi(op2.substr(1, 1), 0, 16);
+            opcode = 0x8000 | ((reg1 & 0xf) << 8) | ((reg2 & 0xf) << 4) | 0x3; // 0x8xy3
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "add") {
+            sline >> op1;
+            sline >> op2;
+            int reg1, reg2;
+            if(op1[0] != 'v' || op2[0] != 'v') {
+                // error
+            }
+            reg1 = std::stoi(op1.substr(1, 1), 0, 16);
+            reg2 = std::stoi(op2.substr(1, 1), 0, 16);
+            opcode = 0x8000 | ((reg1 & 0xf) << 8) | ((reg2 & 0xf) << 4) | 0x4; // 0x8xy4
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "sub") {
+            sline >> op1;
+            sline >> op2;
+            int reg1, reg2;
+            if(op1[0] != 'v' || op2[0] != 'v') {
+                // error
+            }
+            reg1 = std::stoi(op1.substr(1, 1), 0, 16);
+            reg2 = std::stoi(op2.substr(1, 1), 0, 16);
+            opcode = 0x8000 | ((reg1 & 0xf) << 8) | ((reg2 & 0xf) << 4) | 0x5; // 0x8xy5
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "shr") {
+            sline >> op1;
+            sline >> op2;
+            int reg1, reg2;
+            if(op1[0] != 'v' || op2[0] != 'v') {
+                // error
+            }
+            reg1 = std::stoi(op1.substr(1, 1), 0, 16);
+            reg2 = std::stoi(op2.substr(1, 1), 0, 16);
+            opcode = 0x8000 | ((reg1 & 0xf) << 8) | ((reg2 & 0xf) << 4) | 0x6; // 0x8xy6
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "subn") {
+            sline >> op1;
+            sline >> op2;
+            int reg1, reg2;
+            if(op1[0] != 'v' || op2[0] != 'v') {
+                // error
+            }
+            reg1 = std::stoi(op1.substr(1, 1), 0, 16);
+            reg2 = std::stoi(op2.substr(1, 1), 0, 16);
+            opcode = 0x8000 | ((reg1 & 0xf) << 8) | ((reg2 & 0xf) << 4) | 0x7; // 0x8xy7
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "shl") {
+            sline >> op1;
+            sline >> op2;
+            int reg1, reg2;
+            if(op1[0] != 'v' || op2[0] != 'v') {
+                // error
+            }
+            reg1 = std::stoi(op1.substr(1, 1), 0, 16);
+            reg2 = std::stoi(op2.substr(1, 1), 0, 16);
+            opcode = 0x8000 | ((reg1 & 0xf) << 8) | ((reg2 & 0xf) << 4) | 0xe; // 0x8xye
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "sne") {
+            sline >> op1;
+            sline >> op2;
+            int reg1, reg2;
+            if(op1[0] != 'v' || op2[0] != 'v') {
+                // error
+            }
+            reg1 = std::stoi(op1.substr(1, 1), 0, 16);
+            reg2 = std::stoi(op2.substr(1, 1), 0, 16);
+            opcode = 0x9000 | ((reg1 & 0xf) << 8) | ((reg2 & 0xf) << 4); // 0x9xy0
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "ldi") {
+            sline >> op1;
+            int addr = std::stoi(op1);
+            opcode = 0xa000 | (addr & 0x0fff); // 0xannn
+            _writeOpcode(opcode);
+        }
+        else if (mnem == "") {}
+        else if (mnem == "rnd") {
+            sline >> op1;
+            sline >> op2;
+            int reg, val;
+            reg = std::stoi(op1.substr(1, 1), 0, 16);
+            val = std::stoi(op2);
+            opcode = 0xc000 | ((reg & 0xf) << 8) | val & 0xff;
             _writeOpcode(opcode);
         }
     }
@@ -115,6 +240,8 @@ void Assembler::_preparse() {
 
     while (std::getline(src, line)) {
         line = line.substr(0, line.find("#")); // remove comments
+        if (line == "") continue;
+        line = line.substr(line.find_first_not_of(" \t")); // trimming leading whitespaces
 
         std::string firstWord = line.substr(0, line.find(" "));
         if (firstWord.size() <= 1) {
