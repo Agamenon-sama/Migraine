9a1f4fb2d3fddadd9cedc037ab52449d02f37b41
Agamenon <chams0099@gmail.com>
2022-08-10 19:39:50 +0100

    made the debugger interface more readable

--- src/Debugger.cpp
+++ src/Debugger.cpp
@@ -33,30 +33,30 @@ void Debugger::render() {
     }
 
     ImGui::Text("State Registers");
-    ImGui::InputInt("program counter", &stateRegs[0]);
-    ImGui::InputInt("stack pointer", &stateRegs[1]);
-    ImGui::InputInt("address register", &stateRegs[2]);
-    ImGui::InputInt("current opcode", &stateRegs[3]);
-    ImGui::InputInt("delay timer", &stateRegs[4]);
-    ImGui::InputInt("sound timer", &stateRegs[5]);
+    ImGui::InputInt("program counter", &stateRegs[0], 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
+    ImGui::InputInt("stack pointer", &stateRegs[1], 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
+    ImGui::InputInt("address register", &stateRegs[2], 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
+    ImGui::InputInt("current opcode", &stateRegs[3], 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
+    ImGui::InputInt("delay timer", &stateRegs[4], 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
+    ImGui::InputInt("sound timer", &stateRegs[5], 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
 
     ImGui::Text("V Registers");
     if (ImGui::BeginTable("split", 4)) {
         ImGui::TableNextColumn(); ImGui::InputInt("v0", &vRegs[0x0]);
-        ImGui::TableNextColumn(); ImGui::InputInt("v1", &vRegs[0x1]);
-        ImGui::TableNextColumn(); ImGui::InputInt("v2", &vRegs[0x2]);
-        ImGui::TableNextColumn(); ImGui::InputInt("v3", &vRegs[0x3]);
         ImGui::TableNextColumn(); ImGui::InputInt("v4", &vRegs[0x4]);
-        ImGui::TableNextColumn(); ImGui::InputInt("v5", &vRegs[0x5]);
-        ImGui::TableNextColumn(); ImGui::InputInt("v6", &vRegs[0x6]);
-        ImGui::TableNextColumn(); ImGui::InputInt("v7", &vRegs[0x7]);
         ImGui::TableNextColumn(); ImGui::InputInt("v8", &vRegs[0x8]);
-        ImGui::TableNextColumn(); ImGui::InputInt("v9", &vRegs[0x9]);
-        ImGui::TableNextColumn(); ImGui::InputInt("vA", &vRegs[0xa]);
-        ImGui::TableNextColumn(); ImGui::InputInt("vB", &vRegs[0xb]);
         ImGui::TableNextColumn(); ImGui::InputInt("vC", &vRegs[0xc]);
+        ImGui::TableNextColumn(); ImGui::InputInt("v1", &vRegs[0x1]);
+        ImGui::TableNextColumn(); ImGui::InputInt("v5", &vRegs[0x5]);
+        ImGui::TableNextColumn(); ImGui::InputInt("v9", &vRegs[0x9]);
         ImGui::TableNextColumn(); ImGui::InputInt("vD", &vRegs[0xd]);
+        ImGui::TableNextColumn(); ImGui::InputInt("v2", &vRegs[0x2]);
+        ImGui::TableNextColumn(); ImGui::InputInt("v6", &vRegs[0x6]);
+        ImGui::TableNextColumn(); ImGui::InputInt("vA", &vRegs[0xa]);
         ImGui::TableNextColumn(); ImGui::InputInt("vE", &vRegs[0xe]);
+        ImGui::TableNextColumn(); ImGui::InputInt("v3", &vRegs[0x3]);
+        ImGui::TableNextColumn(); ImGui::InputInt("v7", &vRegs[0x7]);
+        ImGui::TableNextColumn(); ImGui::InputInt("vB", &vRegs[0xb]);
         ImGui::TableNextColumn(); ImGui::InputInt("vF", &vRegs[0xf]);
         ImGui::EndTable();
     }
